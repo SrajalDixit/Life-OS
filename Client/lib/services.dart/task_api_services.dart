11d08ae4be56daa0c9ee838d3b51de52a5ef5433
SrajalDixit <srajaldixit01@gmail.com>
2025-06-04 18:06:53 +0530

    tasks retrieve done

--- Client/lib/services.dart/task_api_services.dart
+++ Client/lib/services.dart/task_api_services.dart
@@ -2,11 +2,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String baseUrl = 'http:// 192.168.97.44:8000';
+  static const String baseUrl = 'http://192.168.97.44:8000';
 
 
   static Future<List<dynamic>> fetchTasks() async {
-    final url = Uri.parse('$baseUrl/tasks');
+    final url = await Uri.parse('$baseUrl/tasks');
 
     final response = await http.get(url);
     print('Response status: ${response.statusCode}');
